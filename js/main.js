af790777d7207b17cf9832c12cbab913aefa8eaa
pozniej-znajde-wolne-haslo <aniarafeld@o2.pl>
2023-01-30 22:09:21 +0100

    adds styling with SASS

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const weight = document.getElementById('weight');
 const female = document.getElementById('female');
 const male = document.getElementById('male');
 
-const activityLevel = document.getElementById('activityLevel');
+const factorPAL = document.getElementById('PAL-factor');
 const submitBtn = document.getElementById('btn');
 
 const bmrKcal = document.getElementById('basalMetRate_inKcal');
@@ -18,18 +18,21 @@ function calculateBMR_and_TDEE (event) {
 
   event.preventDefault(); 
 
-  let inputHeight = (height.value).toString().replace(",", "").replace(".", "");
+  let inputHeight = (height.value).replace(",", "").replace(".", "");
   /* oder wenn kleiner als 3 dann * 100 */
   let bmr;
   let tdee;
 
+  if (!(height.value && age.value && weight.value && (female.checked || male.checked) && factorPAL.value)) {
+    window.alert("Überprüfen Sie bitte Ihre Angaben")
+  }
 
   if (female.checked) {
     bmr = 664.7 + (13.7 * weight.value) + (5 * inputHeight) - (6.8 * age.value);
-    tdee = (664.7 + (13.7 * weight.value) + (5 * inputHeight) - (6.8 * age.value)) * activityLevel.value;
+    tdee = (664.7 + (13.7 * weight.value) + (5 * inputHeight) - (6.8 * age.value)) * factorPAL.value;
   } else {
     bmr = 655.1 + (9.6 * weight.value) + (1.8 * inputHeight) - (4.7 * age.value);
-    tdee = 655.1 + (9.6 * weight.value) + (1.8 * inputHeight) - (4.7 * age.value) * activityLevel.value;
+    tdee = (655.1 + (9.6 * weight.value) + (1.8 * inputHeight) - (4.7 * age.value)) * factorPAL.value;
   }
 
   bmrKcal.innerHTML = bmr.toFixed(0);
